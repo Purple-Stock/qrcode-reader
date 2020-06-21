19dad61266b0a15f0f8fcdc5561073a6e7132631
puppe1990 <matheus.puppe90@hotmail.com>
2020-06-20 21:58:44 -0300

    fix: add complete baseurl

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://app.purplestock.com.br',
+  baseURL: 'https://app.purplestock.com.br/api/v1',
 });
 
 export default api;
